d36c46251352f280e7e1aee005cc1c9aca5c14ed
parker <69760520+psmithdev@users.noreply.github.com>
2023-04-07 14:59:42 -0500

    indexOf(), splice() and if statement

--- sandboxQuokka.js
+++ sandboxQuokka.js
@@ -1,27 +1,11 @@
 // let emptyArray = [];
 
-// function removeArray(empty, removeItem) {
-//   return (emptyArray += empty.splice(0, removeItem));
-// }
-
-// console.log(removeArray([1, 2, 3, 4], 4));
-
-const myArray = [1, 2, 3, 4];
-
-const index = myArray.indexOf(3);
-
-const x = myArray.splice(index, 1);
-
-console.log(`myArray values: ${myArray}`);
-console.log(`variable x value is: ${x}`);
-
-let emptyArrayTwo = [];
-
-function removeArrayTwo(emptyTwo, removeItemTwo) {
-  const index = emptyTwo + emptyArrayTwo.indexOf(3);
-  const removeItemTwo = emptyTwo.splice(index, 1);
-  return;
+function removeArray(empty, removeItem) {
+  const index = empty.indexOf(removeItem);
+  if (index !== -1) {
+    empty.splice(index, 1);
+  }
+  return empty;
 }
 
-console.log(removeArrayTwo([1, 2, 3, 4], 3));
-// removeArrayTwo([1,2,3,4], 3);
+console.log(removeArray([1, 2, 3, 4], 3));
