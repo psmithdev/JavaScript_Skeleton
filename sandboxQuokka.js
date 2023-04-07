3a0240bf1954932325b5cab3f722bc12a7f372c9
parker <69760520+psmithdev@users.noreply.github.com>
2023-04-07 14:32:30 -0500

    indexOf() method

--- sandboxQuokka.js
+++ sandboxQuokka.js
@@ -1,9 +1,27 @@
-let emptyArray = [];
+// let emptyArray = [];
 
-function removeArray(empty, removeItem) {
-  //   let sliced = Array.prototype.slice.call(arguments);
-  // let removeItem += empty.splice(0, removeItem);
-  return (emptyArray += empty.splice(0, removeItem));
+// function removeArray(empty, removeItem) {
+//   return (emptyArray += empty.splice(0, removeItem));
+// }
+
+// console.log(removeArray([1, 2, 3, 4], 4));
+
+const myArray = [1, 2, 3, 4];
+
+const index = myArray.indexOf(3);
+
+const x = myArray.splice(index, 1);
+
+console.log(`myArray values: ${myArray}`);
+console.log(`variable x value is: ${x}`);
+
+let emptyArrayTwo = [];
+
+function removeArrayTwo(emptyTwo, removeItemTwo) {
+  const index = emptyTwo + emptyArrayTwo.indexOf(3);
+  const removeItemTwo = emptyTwo.splice(index, 1);
+  return;
 }
 
-console.log(removeArray([1, 2, 3, 4], 3));
+console.log(removeArrayTwo([1, 2, 3, 4], 3));
+// removeArrayTwo([1,2,3,4], 3);
