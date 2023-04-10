3dd350fa9e97997fea179adfb95247dc5b87a202
parker <69760520+psmithdev@users.noreply.github.com>
2023-04-10 10:49:52 -0500

    rest parameter, filter and includes method, arrow

--- sandboxQuokka.js
+++ sandboxQuokka.js
@@ -1,6 +1,4 @@
-// let emptyArray = [];
-
-function removeArray(empty, removeItem) {
+function removeArray(empty, ...removeItem) {
   const index = empty.indexOf(removeItem);
   if (index !== -1) {
     empty.splice(index, 1);
@@ -9,3 +7,9 @@ function removeArray(empty, removeItem) {
 }
 
 console.log(removeArray([1, 2, 3, 4], 3));
+
+function removeFromArray(array, ...args) {
+  return array.filter((x) => !args.includes(x));
+}
+
+console.log(removeFromArray([1, 2, 3, 4], 2, "tacos"));
