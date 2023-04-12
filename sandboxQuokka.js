2e6f7d8d977f299e528159c47f7d3af8e0ca42ef
parker <69760520+psmithdev@users.noreply.github.com>
2023-04-12 14:10:45 -0500

    if else statement pseudocode

--- sandboxQuokka.js
+++ sandboxQuokka.js
@@ -1,9 +1,20 @@
 function sumAll(integerOne, integerTwo) {
   let result = 0;
+
   for (let i = integerOne; i <= integerTwo; i++) {
     result += i;
   }
+
   return result;
 }
 
 console.log(sumAll(123, 1));
+
+if (integerOne >= integerTwo) {
+  // run code
+} else if (integerTwo <= integerTwo) {
+  //run code
+}
+{
+  return (results = "ERROR");
+}
