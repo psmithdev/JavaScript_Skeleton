3014d5921fc0e4abad7db2f8dbbe86bc0c24621b
parkerOffice <69760520+psmithdev@users.noreply.github.com>
2023-08-07 11:36:36 -0500

    cat constructor object basics

--- js.js
+++ js.js
@@ -1,6 +1,13 @@
-const person = {
-  name: "Chris",
-  introduceSelf() {
-    console.log(`Hello, I am ${this.name}`);
-  },
-};
+function Cat(name, breed, color) {
+  this.name = name;
+  this.breed = breed;
+  this.color = color;
+  this.greeting = function () {
+    console.log(`Hello! said ${this.name} the ${this.breed}`);
+  };
+}
+
+const cat1 = new Cat("Bertie", "Cymric", "white");
+const cat2 = new Cat("Elfie", "Giant", "blue");
+cat1.greeting();
+cat2.greeting();
